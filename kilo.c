f53c3c725ee948a6ddb885bf87afe07bfaef6922
Robby Stokoe <robbystk@gmail.com>
2019-05-11 08:55:26 -0600

    Turn off echoing as part of entering raw mode

--- kilo.c
+++ kilo.c
@@ -1,6 +1,23 @@
 #include <unistd.h>
+#include <termios.h>
+
+void enableRawMode() {
+    // Put terminal in raw mode
+    struct termios raw;
+    tcgetattr(STDIN_FILENO, &raw);  // get terminal attribute struct
+
+    // set flags appropriately
+    raw.c_lflag &= ~(ECHO);     // turn of echoing
+
+    // set attributes in the actual interface or whatever
+    // TCSAFLUSH: flush output written, discarding non-read input before change
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
+    enableRawMode();
+
+    // read one character at a time until EOF or 'q' is encountered
     char c;
     while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
