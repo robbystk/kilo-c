552d27a704f972567b30aa637411aa69c035186a
Robby Stokoe <robbystk@gmail.com>
2019-08-20 19:56:20 -0600

    Set timeout and min character count to avoid blocking

--- kilo.c
+++ kilo.c
@@ -26,6 +26,10 @@ void enableRawMode() {
     raw.c_oflag &= ~(OPOST);
     // set character size to 8 bits
     raw.c_cflag |= (CS8);
+    // set timeout for read()
+    raw.c_cc[VTIME] = 1;
+    // set minimum number of characters for read()
+    raw.c_cc[VMIN] = 0;
 
     // set attributes in the actual interface or whatever
     // TCSAFLUSH: flush output written, discarding non-read input before change
@@ -36,13 +40,15 @@ int main() {
     enableRawMode();
 
     // read one character at a time until EOF or 'q' is encountered
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    while (1) {
+        char c = '\0';
+        read(STDIN_FILENO, &c, 1);
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         } else {
             printf("%c (%d)\r\n", c, c);
         }
+        if (c == 'q') break;
     }
     return 0;
 }
