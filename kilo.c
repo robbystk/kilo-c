6f2057c313882798fa881a467ea6a970eade2a6f
Robby Stokoe <robbystk@gmail.com>
2019-08-23 08:59:39 -0600

    Add missing equals sign to if condition

--- kilo.c
+++ kilo.c
@@ -52,7 +52,7 @@ char editorReadKey() {
     int nread;
     char c;
     while ((nread = read(STDIN_FILENO, &c, 1)) == -1) {
-        if (nread = -1 && errno != EAGAIN) die("read");
+        if (nread == -1 && errno != EAGAIN) die("read");
     }
     return c;
 }
