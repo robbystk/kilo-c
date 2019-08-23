4660a9ec1fe6265842d39539f9c079cb69360229
Robby Stokoe <robbystk@gmail.com>
2019-08-23 10:39:22 -0600

    Add an alternate method to determine window size

--- kilo.c
+++ kilo.c
@@ -67,12 +67,33 @@ char editorReadKey() {
     return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    printf("\r\n");
+    while (i < sizeof(buf)) {
+        if (read(STDIN_FILENO, &buf[i], 1) == -1) break;
+        if (buf[i] == 'R') break;
+        i++;
+    }
+
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%dR", rows, cols) != 2) return -1;
+
+    return 0;
+}
 
 int getWinSize(int *rows, int *cols) {
     struct winsize ws;
 
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
-        return -1;
+        if (write(STDOUT_FILENO, "\x1b[999B\x1b[999C", 12) != 12) return -1;
+        return getCursorPosition(rows, cols);
     } else {
         *rows = ws.ws_row; *cols = ws.ws_col;
         return 0;
