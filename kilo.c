e66c71ac24e8be1d475115f0b639bfcb92a902af
Robby Stokoe <robbystk@gmail.com>
2019-08-25 22:48:43 -0600

    Print centered welcome message

--- kilo.c
+++ kilo.c
@@ -9,6 +9,9 @@
 #include <sys/ioctl.h>
 
 /*** defines ***/
+
+#define KILO_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)    // take the 5 least-significant bits of k
 
 /*** data ***/
@@ -128,12 +131,28 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; y++) {
+        if (y == E.screenrows / 3) {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome),
+                    "Kilo editor -- version %s", KILO_VERSION);
+            if (welcomelen > E.screencols) welcomelen = E.screencols;
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding) {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while (padding--) abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomelen);
+        } else {
+            abAppend(ab, "~", 1);
+        }
+
         abAppend(ab, "\x1b[K", 3);
-        abAppend(ab, "~", 1);
 
         if (y < E.screenrows - 1) {
             abAppend(ab, "\r\n", 3);
         }
+
     }
 }
 
